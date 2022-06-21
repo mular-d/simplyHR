05df68da751544da8ff5577567335520f515e5e2
Muluneh Derese <muluneh.derese@protonmail.com>
2022-06-21 09:31:25 +0300

    fix: routing

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -13,7 +13,7 @@ import { NavbarWrapperComponent } from './navbar/navbar-wrapper/navbar-wrapper.c
 
 const routes: Routes = [
   {
-    path: 'job/:id',
+    path: 'job',
     component: JobFormComponent,
   },
   
